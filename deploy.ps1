1500fcdb437ad2b05a58bfcd83972d232b3e8b4e
Gabriel Weyer <gabriel.weyer@readify.net>
2018-01-11 16:48:11 +1100

    Add deploy script

---
+++ deploy.ps1
@@ -0,0 +1,26 @@
+$ErrorActionPreference = "Stop"
+
+$tmpPath = [io.path]::combine([System.IO.Path]::GetTempPath(), "jekyll-$([System.Guid]::NewGuid())")
+
+docker run `
+    --rm `
+    --volume="$($PWD):/srv/jekyll" `
+    --volume="$($tmpPath):/publish" `
+    --workdir="/srv/jekyll" `
+    -it `
+    jekyll/jekyll:3.6.2 `
+    jekyll build
+
+if ($LASTEXITCODE -ne 0) {
+    throw 'Error when building the Jekyll site in Docker'
+}
+
+git checkout master
+git pull --rebase origin master
+
+Copy-Item "$($tmpPath)/*" $PWD -Recurse
+
+git add .
+
+git commit -m "Release"
+git push origin master
